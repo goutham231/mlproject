e3e3f8673bab1e0d833e74830fa8f2becb31ea0a
goutham231 <gt@todoappp.com>
2025-05-28 12:45:32 +0530

    prediction done

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import dill
-#import pickle
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -53,6 +53,13 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,params):
     except Exception as e:
         raise CustomException(e, sys)
 
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
                 
 
             
